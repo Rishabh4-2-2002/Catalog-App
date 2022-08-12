97cf342ba21caec030751631da762a6f071df2fb
Rishabh Handoo <rhandoo_be20@thapar.edu>
2022-08-12 22:55:37 +0530

    data extracted from json

--- catalogapp2/lib/pages/home_page.dart
+++ catalogapp2/lib/pages/home_page.dart
@@ -1,19 +1,52 @@
+import 'dart:convert';
+import 'package:catalogapp2/models/catalog.dart';
 import 'package:catalogapp2/widgets/drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import '../widgets/items_widget.dart';
 
-class HomePage extends StatelessWidget {
-
-
+class HomePage extends StatefulWidget {
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async{
+    await Future.delayed(Duration(seconds: 2));
+    final catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text('Catalog App'),
       ),
-      body: Center(
-        child: Container(
-          child: Text('Welcome'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
+            ? ListView.builder(
+          itemCount: CatalogModel.items?.length,
+          itemBuilder: (context, index) => ItemWidget(
+            item: CatalogModel.items![index],
+          ),
+        )
+            : Center(
+          child: CircularProgressIndicator(),
         ),
       ),
       drawer: MyDrawer(),
